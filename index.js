87b565dbebd0e2223344435aea8c9f1bafe83dd9
Danish Farooqui <danishfarooqui2018@gmail.com>
2024-08-08 20:11:10 +0530

    backend

--- index.js
+++ index.js
@@ -6,3 +6,10 @@ const port=5000;
 
 app.listen(port,() => {console.log('server started')});
 
+
+// request method
+
+// get- used to read data from server
+// post - to add data on server-(basic level)
+// delete - to update data
+// put - to delete data
